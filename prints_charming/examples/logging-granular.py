97e8fabf401f51c760f3c6e36e3b095fbe6927a7
DeeFiFoFun <dev@deefifofun.xyz>
2023-10-23 11:30:28 -0400

    updated the examples directory logging-granular.py minor correction for easier readability. Same for README.md

--- prints_charming/examples/logging-granular.py
+++ prints_charming/examples/logging-granular.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 import logging
-from ..prints_charming import ColorPrinter
+from ..prints_charming import TextStyle, ColorPrinter
 from datetime import datetime
 import time
 
 # To run this script as a module inside the package. Navigate to the top-level directory and run
 # python -m prints_charming.examples.logging-granular
 
+styles = {
+    "default"      : TextStyle(),
+    "white"        : TextStyle(color="white", italic=True),
+    "green"        : TextStyle(color="green"),
+    "vgreen"       : TextStyle(color="vgreen"),
+    "red"          : TextStyle(color="red"),
+    "vred"         : TextStyle(color="vred"),
+    "blue"         : TextStyle(color="blue"),
+    "yellow"       : TextStyle(color="yellow"),
+    "vyellow"      : TextStyle(color="vyellow"),
+    "cyan"         : TextStyle(color="cyan"),
+    "vcyan"        : TextStyle(color="vcyan"),
+    "orange"       : TextStyle(color="orange"),
+}
 
 class ColorPrinterLogHandler(logging.Handler):
     TIMESTAMP_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
@@ -15,7 +29,7 @@ class ColorPrinterLogHandler(logging.Handler):
 
     def __init__(self):
         super().__init__()
-        self.cp = ColorPrinter()
+        self.cp = ColorPrinter(styles=styles)
 
 
     def emit(self, record):
@@ -28,7 +42,7 @@ class ColorPrinterLogHandler(logging.Handler):
         formatted_timestamp = datetime.fromtimestamp(timestamp).strftime(self.TIMESTAMP_FORMAT)
 
         # Define styles for different components
-        timestamp_style = 'magenta'
+        timestamp_style = 'white'
         level_styles = {
             'INFO'    : 'green',
             'WARNING' : 'yellow',
@@ -40,12 +54,13 @@ class ColorPrinterLogHandler(logging.Handler):
         log_level_style = level_styles.get(log_level, 'default')
 
         # Get styled components
-        styled_timestamp = self.cp.apply_style(timestamp_style, f"LOG[{log_level}]")
+        styled_log_level_prefix = self.cp.apply_style(log_level_style, f"LOG[{log_level}]")
+        styled_timestamp = self.cp.apply_style(timestamp_style, formatted_timestamp)
         styled_level = self.cp.apply_style(log_level_style, log_level)
         styled_text = self.cp.apply_style(log_level_style, text)
 
         # Create the final styled message
-        timestamped_message = f"{styled_timestamp}: {formatted_timestamp} {styled_level} - {styled_text}"
+        timestamped_message = f"{styled_log_level_prefix} {styled_timestamp} {styled_level} - {styled_text}"
 
         print(timestamped_message)
 
