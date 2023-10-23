593c35b55a4d083abc95e8e92a62db11ee2cf5c0
DeeFiFoFun <dev@deefifofun.xyz>
2023-10-23 06:58:05 -0400

    updated setup.py to new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='prints_charming',
-    version='1.0.0',
+    version='1.1.0',
     url='https://github.com/deefifofun/prints_charming',
     author='DeeFiFoFun',
     author_email='dev@deefifofun.xyz',
